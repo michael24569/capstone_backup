7f144ec74ecb93c5f0f0ae381631161b818df532
Keykil12 <enozamichael12@gmail.com>
2024-11-24 20:16:44 +0800

    latest to

--- admin_backup.php
+++ admin_backup.php
@@ -78,6 +78,18 @@ unset($_SESSION['backup_file']);
 .main-content {
   text-align: center;
 }
+.input-icon {
+  width: 24px; /* Adjust icon size */
+  height: 24px;
+  fill: black; /* Icon color */
+  flex-shrink: 0;
+  margin-bottom: -22px;
+
+}
+.input-group{
+    padding: 1% 0;
+    position: relative;
+  }
 </style>
 </head>
 <body style="background: #071c14;">
@@ -96,9 +108,12 @@ unset($_SESSION['backup_file']);
 
         <!-- Form for backup submission -->
         <form action="backupCondition.php" method="POST">
+        <div class="input-group">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="input-icon">
+            <path d="M144 144l0 48 160 0 0-48c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192l0-48C80 64.5 144.5 0 224 0s144 64.5 144 144l0 48 16 0c35.3 0 64 28.7 64 64l0 192c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 256c0-35.3 28.7-64 64-64l16 0z"/></svg>
+  </div>
             <div class="input-group">
-            <i class="fas fa-lock"></i>
-                <input type="password" name="password" id="password" placeholder="Enter Password">
+             <input type="password" name="password" id="password" placeholder="Enter Password">
                 <label for="password">Password</label>
             </div>
 
