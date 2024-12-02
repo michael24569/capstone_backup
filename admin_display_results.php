c03204f70ba2e2ada47261f18e8abb85bee6ecac
kyle-12345 <kylecyrus11ambat10@gmail.com>
2024-12-02 15:20:52 +0800

    add logo in records

--- admin_display_results.php
+++ admin_display_results.php
@@ -126,7 +126,7 @@ function displayRecordStatus() {
 
         .summary-table {
             width: 95%;
-            margin: 20px auto;
+            margin: 30px auto;
             border-collapse: collapse;
             margin-left: 70px;
         }
@@ -605,7 +605,7 @@ function displayRecordStatus() {
   #print {
     font-family: "MyFont";
     position: absolute;
-    top: 65px;
+    top:530px;
     right: 10px;
     background-color: #479149;
     color: white;
@@ -721,6 +721,33 @@ p {
 .alert-error {
     background-color: #dc3545;
 }
+
+
+  .seal {
+    text-align: center;
+    }
+
+    .seal img {
+    position:absolute;
+    max-width: 6.5%; 
+    height: auto; 
+    margin-top:-1%;
+    margin-left:-41%;
+    z-index: 10000;
+    }
+    .cctlogo {
+    text-align: center;
+    
+    }
+
+    .cctlogo img {
+    position:absolute;
+    max-width: 6.5%; 
+    height: auto; 
+    margin-top:-1%;
+    margin-left:38%;
+    z-index: 10000;
+    }
     </style>
 </head>
  <link rel="stylesheet" href="style1.css">
@@ -812,6 +839,15 @@ p {
     </ul>
 </div>
 <button id="print" class="print-button"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="input-icon"><!--!Font Awesome Free 6.7.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M168 80c-13.3 0-24 10.7-24 24l0 304c0 8.4-1.4 16.5-4.1 24L440 432c13.3 0 24-10.7 24-24l0-304c0-13.3-10.7-24-24-24L168 80zM72 480c-39.8 0-72-32.2-72-72L0 112C0 98.7 10.7 88 24 88s24 10.7 24 24l0 296c0 13.3 10.7 24 24 24s24-10.7 24-24l0-304c0-39.8 32.2-72 72-72l272 0c39.8 0 72 32.2 72 72l0 304c0 39.8-32.2 72-72 72L72 480zM176 136c0-13.3 10.7-24 24-24l96 0c13.3 0 24 10.7 24 24l0 80c0 13.3-10.7 24-24 24l-96 0c-13.3 0-24-10.7-24-24l0-80zm200-24l32 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-32 0c-13.3 0-24-10.7-24-24s10.7-24 24-24zm0 80l32 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-32 0c-13.3 0-24-10.7-24-24s10.7-24 24-24zM200 272l208 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-208 0c-13.3 0-24-10.7-24-24s10.7-24 24-24zm0 80l208 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-208 0c-13.3 0-24-10.7-24-24s10.7-24 24-24z"/></svg>Print</button>
+    
+<br><div class="seal">
+    <img src ="images/seal.png" alt="Tagaytay City Seal">
+  </div>
+  <div class="cctlogo">
+    <img src ="images/cctlogo.png" alt="Tagaytay City Seal">
+  </div>
+<br>
+
     <h1>Summary of Status Report</h1>
     <br>
     <br>
