aff9f9dbf86ae4c234eb1870d94c53cfc9f42002
Keykil12 <enozamichael12@gmail.com>
2024-12-09 06:11:40 +0800

    updating the backup

--- downloadBackup.php
+++ downloadBackup.php
@@ -8,7 +8,7 @@ if (!isset($_GET['file'])) {
 }
 
 $backup_file_name = urldecode($_GET['file']);
-$backup_directory = 'C:/backup'; // Update your backup directory path
+$backup_directory = 'C:\Users\user\Downloads\Backup Files'; // Update your backup directory path
 
 // Check if the backup has expired (i.e., 30 seconds after it was created)
 if (isset($_SESSION['backup_time']) && (time() - $_SESSION['backup_time']) > 30) {
